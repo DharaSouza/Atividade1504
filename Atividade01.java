70c3133bedf9520ee5c6b51656e816b5611bf08e
DharaSouza <dharana.rodrigues@gmail.com>
2024-05-15 12:22:24 -0300

    15/05

---
+++ Atividade01.java
@@ -0,0 +1,21 @@
+package Vetor;
+
+import java.util.Scanner;
+
+public class Atividade01 {
+
+	public static void main(String[] args) {
+		String a[] = new String[5];
+		Scanner ler = new Scanner(System.in);
+		for (int i = 0; i<5; i++) {
+		System.out.println("informe os nomes:");
+		a[i] = ler.nextLine();
+		}
+		for(int i = 0; i<5; i++) {
+			System.out.println(a[i]);
+		}
+		
+		ler.close();
+
+	}
+}
